6586a156d0db05f800dd4b215b69cfd24d22195d
daurislittle <dauris.little@johnston.biz>
2019-12-03 20:23:42 -0500

    Application completed Consider some changes to make more efficient

--- FileNameChanger_Console/Program.cs
+++ FileNameChanger_Console/Program.cs
@@ -10,30 +10,89 @@ namespace FileNameChanger_Console
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            var directory = new DirectoryInfo(@"C:\Users\Dauris Little\Downloads\Bleach Complete Series + Movies\Season 08; The Arrancar 2; The Fierce Fight");
-            var files = directory.GetFiles();
+            var currentDirectory = @"C:\\Users\\Dauris Little\\";
+            Console.WriteLine("Current Directory Location : C:\\Users\\Dauris Little");
+            ListFilesWithinDirectory(currentDirectory);
 
+            Console.WriteLine("Name change has been successful \n You may now leave the application ");
+            Console.ReadKey();
+        }
+
+        private static void ChangeFilesNames(string findDirectory, FileInfo[] files)
+        {
+            //create a preview of the file names
             var fn = 0;
-            foreach (FileInfo candidate in files)
+            foreach (var file in files)
             {
                 fn++;
-                File.Move(candidate.FullName, candidate.FullName.Replace(candidate.Name, $"S07E{fn}.mkv"));
+                Console.WriteLine($"Current File Name -- {file.Name} changed New File Name -- S07E{fn}");
             }
 
-            Console.Read();
-            var browser = new FolderBrowserDialog();
-            if (browser.ShowDialog() == DialogResult.OK)
+            var nameChangeStatus = Console.ReadLine();
+
+            if (nameChangeStatus== "Yes")
             {
-                Console.WriteLine($"directory : {browser}");
+                fn = 0;
+                foreach (var candidate in files)
+                {
+                    fn++;
+                    File.Move(candidate.FullName, candidate.FullName.Replace(candidate.Name, $"S07E{fn}.mkv"));
+                }
             }
-            Console.WriteLine($"file prefix :");
+            else
+            {
+                Console.WriteLine("Name change has been cancel");
+            }
+            
+        }
 
-            var location = Console.ReadLine();
+        private static void ListFilesWithinDirectory(string findDirectory)
+        {
+            var userSelectedDirectory = Console.ReadLine();
+            var newDirectory = $"{findDirectory}{userSelectedDirectory}\\";
+            if (!string.IsNullOrEmpty(userSelectedDirectory))
+            {
+                var reviewDirectory = new DirectoryInfo($@"{newDirectory}");
+                var directories = reviewDirectory.GetDirectories();
+                var files = reviewDirectory.GetFiles();
+                
+                //this will list out the current level directories 
+                Console.WriteLine("Current Directories");
+                foreach (var directory in directories)
+                {
+                    
+                    Console.WriteLine(directory.Name);
+                }
+                
+                //this will list out the current level of files
+                Console.WriteLine("Current Files");
+                foreach (var file in files)
+                {
+                    Console.WriteLine($"{file.Name}");
+                }
 
-            Console.WriteLine($"your location : {location}");
-            Console.Read();
+                //this recycle then
+                ListFilesWithinDirectory(newDirectory);
+
+            }
+            else
+            {
+                var reviewDirectory = new DirectoryInfo($@"{newDirectory}");
+                var files = reviewDirectory.GetFiles();
+
+                Console.WriteLine("----------Current Files up for renaming----------");
+                foreach (var file in files)
+                {
+                    Console.WriteLine($"{file.Name}");
+                }
+                Console.WriteLine("----------\n Do you wish to proceed");
+                var renameFiles = Console.ReadLine();
+
+                ChangeFilesNames(renameFiles, files);
+            }
         }
     }
 }
